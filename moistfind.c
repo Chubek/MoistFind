aa07f586cfe5d7ba7321761efed8db384d9505f6
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-07-09 03:01:08 +0430

    rm asm

--- moistfind.c
+++ moistfind.c
@@ -12,39 +12,6 @@
 #define MK32X(M) MK16X(M), MK16X(M)
 #define MK60X(M) MK32X(M), MK16X(M), MK8X(M), MK4X(M)
 
-#ifdef NO_ASM_USAGE
-// taken from https://graphics.stanford.edu/~seander/bithacks.html#IntegerLogLookup
-static const BYTE log2lut[UCHAR_MAX + 1] = 
-{
-#define LT(n) n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n
-    -1, 0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3,
-    LT(4), LT(5), LT(5), LT(6), LT(6), LT(6), LT(6),
-    LT(7), LT(7), LT(7), LT(7), LT(7), LT(7), LT(7), LT(7)
-};
-#define GET_LOG_TWO(DST, SRC) do { UINT32 v = SRC & 0xfe; unsigned r; register UINT32 t, tt; if (tt = v >> 16) {  r = (t = tt >> 8) ? 24 + log2lut[t] : 16 + log2lut[tt]; } else {  r = (t = v >> 8) ? 8 + log2lut[t] : log2lut[v]; }; DST = r; } while (0)
-#define ZERO_OUT_MEMORY(DST, LEN) memset(DST, 0, LEN)
-#else
-#ifdef __aarch64__
-#define ASM_COUNT_TZ "ctz %0, %1"
-#define ASM_MEM_ZERO "mov x9, %0; __zero: strb wzr, [%0], #1; subs %1, %1, #1; b.ne __zero;"
-#define ASM_ZERO_MEM_INPUT(PTR, LEN) "r" (PTR), "r" (LEN)
-#define CLOBBER_MEM_ZERO "x9", "x10", "memory"
-#elif __amd64__
-#define ASM_COUNT_TZ "tzcnt %1, %0"
-#define ASM_MEM_ZERO "movq %0, %%r11; xorq %%r12, %%r12; __zero: movb %%r12b, (%%r11); incq %%r11; decq %1; jnz __zero;"
-#define ASM_ZERO_MEM_INPUT(PTR, LEN) "g" (PTR), "r" (LEN)
-#define CLOBBER_MEM_ZERO "r11", "rbx", "memory"
-#else
-#error "Please pass -D NO_ASM_USAGE"
-#endif
-#define GET_LOG_TWO(DST, SRC) do { uINT3264_t dst = 0, src = SRC & 0xfe; asm volatile (ASM_COUNT_TZ : "=g" (dst) : "g" (src)); DST = dst; } while (0)
-#define ZERO_OUT_MEMORY(DST, LEN) do { uINT32ptr_t ptr = (uINT32ptr_t)DST; size_t len = LEN; asm volatile (ASM_MEM_ZERO : : ASM_ZERO_MEM_INPUT(ptr, len) : CLOBBER_MEM_ZERO ); } while (0)
-#endif
-
-#define OR_SHR(VAL, SHRN) VAL |= VAL >> SHRN
-#define NEAREST_SQUARE(VAL) do { VAL--; OR_SHR(VAL, 1); OR_SHR(VAL, 2); OR_SHR(VAL, 4); OR_SHR(VAL, 8); OR_SHR(VAL, 16); OR_SHR(VAL, 32); VAL++;  } while (0)
-#define GET_FIRST_PERIOD(DST, SRC, LEN) do { register BYTE *cpy; for (cpy = &SRC[0]; *cpy && *(cpy - 1) != '.'; ++cpy); DST = (INT32)(cpy - SRC) == LEN ? -1 : (INT32)(cpy - SRC); } while (0)
-
 #define DEFAULT_RETURN return TRUE
 
 #define INLINE_LOCAL static inline
@@ -53,6 +20,19 @@ static const BYTE log2lut[UCHAR_MAX + 1] =
 #define EXTERNAL_LINKAGE extern
 #define EXTERNAL_INLINE extern inline
 #define NOARGS void
+
+
+REENTRANT_NOCHANGE BYTE log2lut[UCHAR_MAX + 1] = {
+	-1, 0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3,
+	MK16X(4), MK16X(5), MK16X(5), MK16X(6), MK16X(6), MK16X(6), MK16X(6),
+	MK16X(7), MK16X(7), MK16X(7), MK16X(7), MK16X(7), MK16X(7), MK16X(7),
+	MK16X(7),
+};
+#define GET_LOG_TWO(DST, SRC) do { UINT32 v = SRC & 0xfe; unsigned r; register UINT32 t, tt; if (tt = v >> 16) {  r = (t = tt >> 8) ? 24 + log2lut[t] : 16 + log2lut[tt]; } else {  r = (t = v >> 8) ? 8 + log2lut[t] : log2lut[v]; }; DST = r; } while (0)
+#define ZERO_OUT_MEMORY(DST, LEN) memset(DST, 0, LEN)
+#define OR_SHR(VAL, SHRN) VAL |= VAL >> SHRN
+#define NEAREST_SQUARE(VAL) do { VAL--; OR_SHR(VAL, 1); OR_SHR(VAL, 2); OR_SHR(VAL, 4); OR_SHR(VAL, 8); OR_SHR(VAL, 16); OR_SHR(VAL, 32); VAL++;  } while (0)
+#define GET_FIRST_PERIOD(DST, SRC, LEN) do { register BYTE *cpy; for (cpy = &SRC[0]; *cpy && *(cpy - 1) != '.'; ++cpy); DST = (INT32)(cpy - SRC) == LEN ? -1 : (INT32)(cpy - SRC); } while (0)
 
 
 typedef BYTE arru8FileName[NAME_MAX];
@@ -67,10 +47,10 @@ typedef struct {
 	UINT16 length;
 	UINT8 extpos;
 	UINT8 lclass;
-} bftFileNameAttrs, *bfpFileNameAttrs;
+} mstFileNameAttrs, *mspFileNameAttrs;
 
 typedef struct {
-	bftFileNameAttrs nmattrs;
+	mstFileNameAttrs nmattrs;
 	ptru8NameStr nmptr;
 } mstFileNameDesc, *mspFileNameDesc;
 
@@ -93,28 +73,28 @@ INLINE_LOCAL boolYield fniInitFileNameDesc(arru8FileName repr, mspFileNameDesc f
 
 typedef BYTE primu8Symbol, *ptru8Symbol, arru8RegexExpr[MAX_REGEX_LEN], *ptru8RegexExpr;
 
-typedef enum {
-	KleeneStarUnary = 2,
-	AtLeastOneUnary = 4,
-	OneOrZeroUnary = 8,
-	EitherOrBinary = 16,
-	NotAnOperator = 0,
-} enumRegexOperator;
-
 typedef struct {
 	UINT32 symbol_b : 8;
 	UINT32 symbol_a : 8;
 	UINT32 operator : 16;
 } bftPartialNFA, *bfpPartialNFA;
 
-INLINE_LOCAL fniInitPartialNFA(bfpPartialNFA fragment, ptru8RegexExpr exprptr) {
-	REENTRANT_NOCHANGE WORD hmapOperatorsMap[UCHAR_MAX] = {
-		MK32X(SENTINEL_SYMBOL), MK8X(SENTINEL_SYMBOL), MK2X(SENTINEL_SYMBOL),
-		ZERO_OR_MORE_SYMBOL, ATLEAST_ONE_SYMBOL, MK16X(SENTINEL_SYMBOL),
-		MK2X(SENTINEL_SYMBOL), SENTINEL_SYMBOL, ONE_OR_ZERO_SYMBOL, MK60X(SENTINEL_SYMBOL),
-		EITHER_OR_SYMBOL, MK6X(SENTINEL_SYMBOL),
+INLINE_LOCAL fniInitPartialNFA(bfpPartialNFA fragment, ptru8RegexExpr exprat) {
+	REENTRANT_NOCHANGE WORD arri32sOperatorsMap[UCHAR_MAX] = {
+		MK32X(SENTINEL_SYMBOL), 
+		MK8X(SENTINEL_SYMBOL), 
+		MK2X(SENTINEL_SYMBOL),
+		ZERO_OR_MORE_SYMBOL, 
+		ATLEAST_ONE_SYMBOL, 
+		MK16X(SENTINEL_SYMBOL),
+		MK2X(SENTINEL_SYMBOL), 
+		SENTINEL_SYMBOL, 
+		ONE_OR_ZERO_SYMBOL, 
+		MK60X(SENTINEL_SYMBOL),
+		EITHER_OR_SYMBOL, 
+		MK6X(SENTINEL_SYMBOL),
 	};
-	fragment->symbol_a = *exprptr;
-	fragment->symbol_b = *(exprptr + 2)
-	fragment->operator = hmapOperatorsMap[*(exprptr + 1)];
+	fragment->symbol_a = *exprat;
+	fragment->symbol_b = *(exprat + 2)
+	fragment->operator = arri32sOperatorsMap[*(exprat + 1)];
 }
